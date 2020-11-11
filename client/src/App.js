5d6ef1ac760ba019d517d113acff327f3059777f
Estiven Salazar <estivensal7@gmail.com>
2020-11-10 22:47:03 -0500

    Created the create-react-app structure, and imported all essential material-ui cdn links & packages. Also began implementing the navigation-drawer.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// importing components
+import Navigation from "./components/layouts/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
