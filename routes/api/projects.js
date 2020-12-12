9fab3b01b3c11bbafafaa04108002a9cfc586280
estivensal7 <estivensal7@gmail.com>
2020-12-12 16:08:31 -0500

    Added the following routes to the projects directory: 'Add Properties to Model' & 'Delete Property from Models'

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -9,255 +9,359 @@ const Project = require("../../models/Project");
 // @desc     Get current user's projects
 // @access   Private
 router.get("/me", auth, async (req, res) => {
-  try {
-    const projects = await Project.find({
-      user: req.user.id,
-    }).populate("user", ["name"]);
+	try {
+		const projects = await Project.find({
+			user: req.user.id,
+		}).populate("user", ["name"]);
 
-    if (!projects) {
-      return res.status(400).json({ msg: "This user has no projects." });
-    }
+		if (!projects) {
+			return res.status(400).json({ msg: "This user has no projects." });
+		}
 
-    res.json(projects);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+		res.json(projects);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
 });
 
 // @route    POST api/projects
 // @desc     Create a project
 // @access   Private
 router.post(
-  "/",
-  auth,
-  check("title", "Title is required").not().isEmpty(),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	"/",
+	auth,
+	check("title", "Title is required").not().isEmpty(),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    // destructure the request
-    const {
-      title,
-      // spread the rest of the fields we don't need to check
-      ...rest
-    } = req.body;
+		// destructure the request
+		const {
+			title,
+			// spread the rest of the fields we don't need to check
+			...rest
+		} = req.body;
 
-    // build a project
-    const projectFields = new Project({
-      user: req.user.id,
-      title: req.body.title,
-      ...rest,
-    });
+		// build a project
+		const projectFields = new Project({
+			user: req.user.id,
+			title: req.body.title,
+			...rest,
+		});
 
-    try {
-      // Using upsert option (creates new doc if no match is found):
-      let project = await projectFields.save();
-      return res.json(project);
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).send("Server Error");
-    }
-  }
+		try {
+			// Using upsert option (creates new doc if no match is found):
+			let project = await projectFields.save();
+			return res.json(project);
+		} catch (err) {
+			console.error(err.message);
+			return res.status(500).send("Server Error");
+		}
+	}
 );
 
 // @route    GET api/projects/:project_id
 // @desc     Get project by ID
 // @access   Private
 router.get("/:project_id", auth, async ({ params: { project_id } }, res) => {
-  try {
-    const project = await Project.findOne({
-      _id: project_id,
-    });
+	try {
+		const project = await Project.findOne({
+			_id: project_id,
+		});
 
-    if (!project) return res.status(400).json({ msg: "Project not found" });
+		if (!project) return res.status(400).json({ msg: "Project not found" });
 
-    return res.json(project);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({ msg: "Server error" });
-  }
+		return res.json(project);
+	} catch (err) {
+		console.error(err.message);
+		return res.status(500).json({ msg: "Server error" });
+	}
 });
 
 // @route    DELETE api/projects/:project_id
 // @desc     Delete project
 // @access   Private
 router.delete("/:project_id", auth, async (req, res) => {
-  try {
-    // Remove project
-    await Project.findOneAndRemove({ _id: req.params.project_id });
+	try {
+		// Remove project
+		await Project.findOneAndRemove({ _id: req.params.project_id });
 
-    res.json({ msg: "Project deleted." });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+		res.json({ msg: "Project deleted." });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
 });
 
 // @route    POST api/projects/models/:project_id
 // @desc     Add models to project
 // @access   Private
 router.post(
-  "/models/:project_id",
-  [auth, [check("name", "Model's Name is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	"/models/:project_id",
+	[auth, [check("name", "Model's Name is required").not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { name, description } = req.body;
+		const { name, description } = req.body;
 
-    const { propName, propType, requiredProp } = req.body.properties[0];
+		let newModel = {
+			name,
+			description,
+			properties: [],
+		};
 
-    const newModel = {
-      name,
-      description,
-      properties: [
-        {
-          propName,
-          propType,
-          requiredProp,
-        },
-      ],
-    };
+		console.log(req.body.properties);
 
-    try {
-      const project = await Project.findOne({ _id: req.params.project_id });
+		for (let i = 0; i < req.body.properties.length; i++) {
+			let newProperty = {
+				propName: req.body.properties[i].propName,
+				propType: req.body.properties[i].propType,
+				requiredProp: req.body.properties[i].requiredProp,
+			};
 
-      project.models.unshift(newModel);
+			newModel.properties.unshift(newProperty);
+			console.log(newModel);
+		}
 
-      await project.save();
+		try {
+			const project = await Project.findOne({
+				_id: req.params.project_id,
+			});
 
-      res.json(project);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
+			project.models.push(newModel);
+
+			await project.save();
+
+			res.json(project);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
 );
 
 // @route    DELETE api/projects/models/:project_id/:model_id
 // @desc     Delete model from project
 // @access   Private
 router.delete("/models/:project_id/:model_id", auth, async (req, res) => {
-  try {
-    const foundProject = await Project.findOne({ _id: req.params.project_id });
-    foundProject.models = foundProject.models.filter(
-      (model) => model._id.toString() !== req.params.model_id
-    );
-    await foundProject.save();
-    return res.status(200).json(foundProject);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ msg: "Server error" });
-  }
+	try {
+		const foundProject = await Project.findOne({
+			_id: req.params.project_id,
+		});
+		foundProject.models = foundProject.models.filter(
+			(model) => model._id.toString() !== req.params.model_id
+		);
+		await foundProject.save();
+		return res.status(200).json(foundProject);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ msg: "Server error" });
+	}
 });
+
+// @route    POST api/projects/models/properties/:project_id/:model_id
+// @desc     Add properties to model in project
+// @access   Private
+router.post(
+	"/models/properties/:project_id/:model_id",
+	[
+		auth,
+		[check("propName", "Propertys Name is required").not().isEmpty()],
+		[check("propType", "Propertys Type is required").not().isEmpty()],
+		[
+			check("requiredProp", "Please specify if this Property is required")
+				.not()
+				.isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { propName, propType, requiredProp } = req.body;
+
+		let newProperty = {
+			propName,
+			propType,
+			requiredProp,
+		};
+
+		try {
+			const project = await Project.findOne({
+				_id: req.params.project_id,
+			});
+
+			let modelIndex = 0;
+			for (let i = 0; i < project.models.length; i++) {
+				if (project.models[i]._id == req.params.model_id) {
+					modelIndex = i;
+				}
+			}
+
+			project.models[modelIndex].properties.push(newProperty);
+
+			await project.save();
+
+			res.json(project);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+// @route    DELETE api/projects/models/properties/:project_id/:model_id/:property_id
+// @desc     Delete property from model
+// @access   Private
+router.delete(
+	"/models/properties/:project_id/:model_id/:property_id",
+	auth,
+	async (req, res) => {
+		try {
+			const project = await Project.findOne({
+				_id: req.params.project_id,
+			});
+
+			let modelIndex = 0;
+			for (let i = 0; i < project.models.length; i++) {
+				if (project.models[i]._id == req.params.model_id) {
+					modelIndex = i;
+				}
+			}
+
+			project.models[modelIndex].properties = project.models[
+				modelIndex
+			].properties.filter(
+				(property) => property._id.toString() !== req.params.property_id
+			);
+
+			await project.save();
+			return res.status(200).json(project);
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({ msg: "Server error" });
+		}
+	}
+);
 
 // @route    POST api/projects/routes/:project_id
 // @desc     Add routes to project
 // @access   Private
 router.post(
-  "/routes/:project_id",
-  [auth, [check("name", "Route's Name is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	"/routes/:project_id",
+	[auth, [check("name", "Route's Name is required").not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { name, description, operation, path } = req.body;
+		const { name, description, operation, path } = req.body;
 
-    const newRoute = {
-      name,
-      description,
-      operation,
-      path,
-    };
+		const newRoute = {
+			name,
+			description,
+			operation,
+			path,
+		};
 
-    try {
-      const project = await Project.findOne({ _id: req.params.project_id });
+		try {
+			const project = await Project.findOne({
+				_id: req.params.project_id,
+			});
 
-      project.routes.unshift(newRoute);
+			project.routes.push(newRoute);
 
-      await project.save();
+			await project.save();
 
-      res.json(project);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
+			res.json(project);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
 );
 
 // @route    DELETE api/projects/routes/:project_id/:route_id
 // @desc     Delete route from project
 // @access   Private
 router.delete("/routes/:project_id/:route_id", auth, async (req, res) => {
-  try {
-    const foundProject = await Project.findOne({ _id: req.params.project_id });
-    foundProject.routes = foundProject.routes.filter(
-      (route) => route._id.toString() !== req.params.route_id
-    );
-    await foundProject.save();
-    return res.status(200).json(foundProject);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ msg: "Server error" });
-  }
+	try {
+		const foundProject = await Project.findOne({
+			_id: req.params.project_id,
+		});
+		foundProject.routes = foundProject.routes.filter(
+			(route) => route._id.toString() !== req.params.route_id
+		);
+		await foundProject.save();
+		return res.status(200).json(foundProject);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ msg: "Server error" });
+	}
 });
 
 // @route    POST api/projects/packages/:project_id
 // @desc     Add packages to project
 // @access   Private
 router.post(
-  "/packages/:project_id",
-  [auth, [check("name", "Packages's Name is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	"/packages/:project_id",
+	[auth, [check("name", "Packages's Name is required").not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { name, documentationLink } = req.body;
+		const { name, documentationLink } = req.body;
 
-    const newPackage = {
-      name,
-      documentationLink,
-    };
+		const newPackage = {
+			name,
+			documentationLink,
+		};
 
-    try {
-      const project = await Project.findOne({ _id: req.params.project_id });
+		try {
+			const project = await Project.findOne({
+				_id: req.params.project_id,
+			});
 
-      project.packages.unshift(newPackage);
+			project.packages.unshift(newPackage);
 
-      await project.save();
+			await project.save();
 
-      res.json(project);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
+			res.json(project);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
 );
 
 // @route    DELETE api/projects/packages/:project_id/:package_id
 // @desc     Delete package from project
 // @access   Private
 router.delete("/packages/:project_id/:package_id", auth, async (req, res) => {
-  try {
-    const foundProject = await Project.findOne({ _id: req.params.project_id });
-    foundProject.packages = foundProject.packages.filter(
-      (package) => package._id.toString() !== req.params.package_id
-    );
-    await foundProject.save();
-    return res.status(200).json(foundProject);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ msg: "Server error" });
-  }
+	try {
+		const foundProject = await Project.findOne({
+			_id: req.params.project_id,
+		});
+		foundProject.packages = foundProject.packages.filter(
+			(package) => package._id.toString() !== req.params.package_id
+		);
+		await foundProject.save();
+		return res.status(200).json(foundProject);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ msg: "Server error" });
+	}
 });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -13,82 +13,82 @@ const Project = require("../../models/Project");
 // @desc     Register user
 // @access   Public
 router.post(
-  "/",
-  [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please include a valid email").isEmail(),
+		check(
+			"password",
+			"Please enter a password with 6 or more characters"
+		).isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { name, email, password } = req.body;
+		const { name, email, password } = req.body;
 
-    try {
-      let user = await User.findOne({ email });
+		try {
+			let user = await User.findOne({ email });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
-      }
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
 
-      user = new User({
-        name,
-        email,
-        password,
-      });
+			user = new User({
+				name,
+				email,
+				password,
+			});
 
-      const salt = await bcrypt.genSalt(10);
+			const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salt);
+			user.password = await bcrypt.hash(password, salt);
 
-      await user.save();
+			await user.save();
 
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: "5 days" },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
-    }
-  }
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: "5 days" },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
 );
 
 // @route    DELETE api/users
-// @desc     Delete user their projects
+// @desc     Delete user and their projects
 // @access   Private
 router.delete("/", auth, async (req, res) => {
-  try {
-    // Remove user's projects
-    await Project.deleteMany({ user: req.user.id });
+	try {
+		// Remove user's projects
+		await Project.deleteMany({ user: req.user.id });
 
-    // Remove user
-    await User.findOneAndRemove({ _id: req.user.id });
+		// Remove user
+		await User.findOneAndRemove({ _id: req.user.id });
 
-    res.json({ msg: "User deleted" });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+		res.json({ msg: "User deleted" });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
 });
 
 module.exports = router;
